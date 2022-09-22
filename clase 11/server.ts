f467db6a411dc041e081e3c08e81e671e8d712b5
Jacimar <jacintorobmartinez@gmail.com>
2022-09-22 08:32:31 -0300

    subo carpeta al main del repo

---
+++ clase 11/server.ts
@@ -0,0 +1,51 @@
+import dotenv from "dotenv"; //importa
+dotenv.config(); //configura
+
+import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose"; 
+import router from "./routes";
+
+const app: Express = express();
+
+app.use(express.json({ limit: "10mb" })); //envio cosas desde postman
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', router)
+
+//setea el servidor... escucha el puerto y levanta el servidor
+app.listen(process.env.PORT, () => {
+  console.log(`Server is up and running at port ${process.env.PORT}`);
+});
+
+app.get("/", (req, res)=>{
+res.send("Suprema con Papas")
+})
+
+interface User{
+  name:string,
+  lastName: string
+}
+
+const Users: User[]=[]
+
+app.post("/", (req:Request<any,any,User>, res) => {
+  Users.push(req.body)
+  res.send(JSON.stringify({data:Users}))
+})
+
+connectToDb()
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.log(err));
+
+async function connectToDb() {
+  if (process.env.DB_CONNECTION_STRING) {
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
+  } else {
+    console.log("Connection string is missing");
+  }
+}
+
+
+/*app.get("/:id", (req, res) => {
+  const blodId = req.params.id
+})*/
+
